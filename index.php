be331d2e3fd1b1bd94dbdaa8004ad86831bb648f
vaishali <vaishali61295@gmail.com>
2021-03-26 22:45:59 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,4 +5,13 @@ include_once("StarWars.html");
 include_once("script.js");
 include_once("Style.css");
 
+include_once("Lukeskywaker.html");
+include_once("Owenlars.html");
+include_once("leiaorgana.html");
+include_once("Obi-wankenovi.html");
+include_once("Darthvader.html");
+include_once("C-3PO.html");
+include_once("BeruWhitesun.html");
+include_once("Biggsdarklighter.html");
+
 ?>
